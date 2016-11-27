507b9e72bf06e1edb73391a9659d94d8e0d4e220
Lucas Calazans <contat@lucascalazans.com.br>
2016-11-27 14:01:06 -0300

    Logs para registro do SW

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 if('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/ssw.js',
 		{scope: '/'}).then(function(reg) {
-		// Service worker registrado!
+		console.log("Service Worker registrado");
 	}).catch(function(error) {
-		// Não foi possível  registrar o Service Worker
 		console.log(error);
 	});
 }
