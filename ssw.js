78c1b7b540298a555c25340d1d0dcf28c9b9ebf2
Lucas Calazans <contat@lucascalazans.com.br>
2016-11-27 14:44:35 -0300

    Mudança no SW

--- ssw.js
+++ ssw.js
@@ -11,10 +11,11 @@ this.addEventListener('install', function(event) {
 	);
 });
 
-this.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			return response || fetch(event.request);
-		})
-	);
-});
+this.onfetch = function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      console.log("fetch failure", event.request.url);
+      return new Response("Request failed!");
+    })
+  );
+}
