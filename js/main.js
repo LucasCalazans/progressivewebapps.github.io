c1d6ce6d571d4113aa8e2e3dddf9adbb8a14d9d8
Lucas Calazans <contat@lucascalazans.com.br>
2016-11-27 14:00:04 -0300

    Adicionando service worker

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("teste");
+if('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/ssw.js',
+		{scope: '/'}).then(function(reg) {
+		// Service worker registrado!
+	}).catch(function(error) {
+		// Não foi possível  registrar o Service Worker
+		console.log(error);
+	});
+}
